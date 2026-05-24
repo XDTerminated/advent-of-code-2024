05cd2538b7c440812322e3b1570c4bcb01770a44
XDTerminated <gupta.sayam2006@gmail.com>
2026-05-23 22:38:39 -0500

    cleaned up day 1 and day 2

--- day01.py
+++ day01.py
@@ -1,47 +1,20 @@
-def part1():
-    left_list = []
-    right_list = []
-
-    while True:
-        line = input()
-        if not line:
-            break
-
-        left, right = map(int, line.split())
-        left_list.append(left)
-        right_list.append(right)
-
-    left_list.sort()
-    right_list.sort()
-
-    sum_of_differences = 0
-
-    for i in range(len(left_list)):
-        sum_of_differences += abs(left_list[i] - right_list[i])
-
-    return sum_of_differences
+def take_input():
+    left, right = [], []
+    while line := input():
+        l, r = map(int, line.split())
+        left.append(l)
+        right.append(r)
+    return sorted(left), sorted(right)
 
 
-def part2():
-    left_list = []
-    right_list = []
-
-    while True:
-        line = input()
-        if not line:
-            break
-
-        left, right = map(int, line.split())
-        left_list.append(left)
-        right_list.append(right)
-
-    similarity_score = 0
-
-    for i in range(len(set(left_list))):
-        similarity_score += left_list[i] * right_list.count(left_list[i])
-
-    return similarity_score
+def part1(left, right):
+    return sum(abs(l - r) for l, r in zip(left, right))
 
 
-print(part1())
-print(part2())
+def part2(left, right):
+    return sum(n * right.count(n) for n in left)
+
+
+left, right = take_input()
+print(part1(left, right))
+print(part2(left, right))
--- day02.py
+++ day02.py
@@ -1,63 +1,28 @@
-input_data = []
-safe = 0
-
-while True:
-    line = input()
-    if not line:
-        break
-
-    row = [int(x) for x in line.split()]
-    input_data.append(row)
+def take_input():
+    reports = []
+    while line := input():
+        reports.append([int(x) for x in line.split()])
+    return reports
 
 
 def is_safe(levels):
-    if len(levels) <= 1:
-        return False
-
-    is_increasing = True
-    is_decreasing = True
-
-    for i in range(len(levels) - 1):
-        diff = levels[i + 1] - levels[i]
-
-        if not (1 <= abs(diff) <= 3):
-            return False
-
-        if diff > 0:
-            is_decreasing = False
-        if diff < 0:
-            is_increasing = False
-
-        if not (is_increasing or is_decreasing):
-            return False
-
-    return True
+    diffs = [b - a for a, b in zip(levels, levels[1:])]
+    return all(1 <= abs(d) <= 3 for d in diffs) and (
+        all(d > 0 for d in diffs) or all(d < 0 for d in diffs)
+    )
 
 
-safe = 0
-for row in input_data:
-    if is_safe(row):
-        safe += 1
-
-print(safe)
+def part1(reports):
+    return sum(is_safe(r) for r in reports)
 
 
-def is_safe_2(levels):
-    if is_safe(levels):
-        return True
-
-    for i in range(len(levels)):
-        dampened = levels[:i] + levels[i + 1 :]
-
-        if is_safe(dampened):
-            return True
-
-    return False
+def part2(reports):
+    return sum(
+        is_safe(r) or any(is_safe(r[:i] + r[i + 1 :]) for i in range(len(r)))
+        for r in reports
+    )
 
 
-safe = 0
-for row in input_data:
-    if is_safe_2(row):
-        safe += 1
-
-print(safe)
+reports = take_input()
+print(part1(reports))
+print(part2(reports))
