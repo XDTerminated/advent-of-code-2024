57089c9e692e55d5aff757d3fe4554a67a0b7651
XDTerminated <gupta.sayam2006@gmail.com>
2026-05-23 22:41:53 -0500

    cleaned up day 3

--- day03.py
+++ day03.py
@@ -1,37 +1,33 @@
 import re
 
-multsum = 0
+MUL = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
+INSTRUCTION = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)")
 
-with open("input.txt", "r") as file:
-    data = file.readlines()
 
-for i in range(len(data)):
-    data[i] = data[i].strip()
-    a = re.findall(r"mul\([0-9]{1,3},[0-9]{1,3}\)", data[i])
+def take_input():
+    lines = []
+    while line := input():
+        lines.append(line)
+    return "".join(lines)
 
-    for j in a:
-        b = j.split("(")[1].split(")")[0].split(",")
-        multsum += int(b[0]) * int(b[1])
 
-print(multsum)
+def part1(memory):
+    return sum(int(a) * int(b) for a, b in MUL.findall(memory))
 
-multsum = 0
 
-mult = True
+def part2(memory):
+    total, enabled = 0, True
+    for m in INSTRUCTION.finditer(memory):
+        token = m.group(0)
+        if token == "do()":
+            enabled = True
+        elif token == "don't()":
+            enabled = False
+        elif enabled:
+            total += int(m.group(1)) * int(m.group(2))
+    return total
 
-for i in range(len(data)):
-    data[i] = data[i].strip()
-    a = re.findall(r"mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don\'t\(\)", data[i])
 
-    for j in a:
-        if j == "do()":
-            mult = True
-
-        elif j == "don't()":
-            mult = False
-
-        elif mult:
-            b = j.split("(")[1].split(")")[0].split(",")
-            multsum += int(b[0]) * int(b[1])
-
-print(multsum)
+memory = take_input()
+print(part1(memory))
+print(part2(memory))
