ea3f004fb204132cfdf48a700976b7ee9375a41a
XDTerminated <gupta.sayam2006@gmail.com>
2026-05-24 14:10:48 -0500

    cleaned up day 13

--- day13.py
+++ day13.py
@@ -1,38 +1,51 @@
-button_a = []
-button_b = []
-prizes = []
-
-with open("input.txt") as file:
-    for line in file:
-        line = line.strip()
-        if line.startswith("Button A:"):
-            x, y = map(int, line.split("X+")[1].split(", Y+"))
-            button_a.append((x, y))
-        elif line.startswith("Button B:"):
-            x, y = map(int, line.split("X+")[1].split(", Y+"))
-            button_b.append((x, y))
-        elif line.startswith("Prize:"):
-            x, y = map(int, line.split("X=")[1].split(", Y="))
-            prizes.append((x, y))
-
-tokens = 0
-
-for i in range(len(button_a)):
-    matrix = [
-        [button_a[i][0], button_a[i][1]],
-        [button_b[i][0], button_b[i][1]],
-        [prizes[i][0] + 10000000000000, prizes[i][1] + 10000000000000],
-    ]
-
-    det_matrix = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-    det_x = matrix[2][0] * matrix[1][1] - matrix[2][1] * matrix[1][0]
-    det_y = matrix[0][0] * matrix[2][1] - matrix[0][1] * matrix[2][0]
-
-    x = det_x / det_matrix
-    y = det_y / det_matrix
-
-    if int(x) == x and int(y) == y and x >= 0 and y >= 0:
-        tokens += int(x) * 3 + int(y)
+import re
 
 
-print(tokens)
+def take_input():
+    nums = []
+    prev_blank = False
+    while True:
+        line = input()
+        if not line:
+            if prev_blank:
+                break
+            prev_blank = True
+            continue
+        prev_blank = False
+        nums.extend(int(x) for x in re.findall(r"\d+", line))
+    machines = []
+    for i in range(0, len(nums), 6):
+        ax, ay, bx, by, px, py = nums[i : i + 6]
+        machines.append(((ax, ay), (bx, by), (px, py)))
+    return machines
+
+
+def solve(machines, offset=0):
+    total = 0
+    for (ax, ay), (bx, by), (px, py) in machines:
+        px += offset
+        py += offset
+        det = ax * by - ay * bx
+        if det == 0:
+            continue
+        a_num = px * by - py * bx
+        b_num = ax * py - ay * px
+        if a_num % det or b_num % det:
+            continue
+        a, b = a_num // det, b_num // det
+        if a >= 0 and b >= 0:
+            total += 3 * a + b
+    return total
+
+
+def part1(machines):
+    return solve(machines)
+
+
+def part2(machines):
+    return solve(machines, offset=10**13)
+
+
+machines = take_input()
+print(part1(machines))
+print(part2(machines))
