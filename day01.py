32fe955dab92633237f4130e0d0db0af9a9166fc
XDTerminated <gupta.sayam2006@gmail.com>
2026-05-23 22:13:25 -0500

    cleanup day 01

--- day01.py
+++ day01.py
@@ -1,29 +1,47 @@
-left_list = []
-right_list = []
+def part1():
+    left_list = []
+    right_list = []
+
+    while True:
+        line = input()
+        if not line:
+            break
+
+        left, right = map(int, line.split())
+        left_list.append(left)
+        right_list.append(right)
+
+    left_list.sort()
+    right_list.sort()
+
+    sum_of_differences = 0
+
+    for i in range(len(left_list)):
+        sum_of_differences += abs(left_list[i] - right_list[i])
+
+    return sum_of_differences
 
 
-while True:
-    line = input()
-    if not line:
-        break
+def part2():
+    left_list = []
+    right_list = []
 
-    left, right = map(int, line.split())
-    left_list.append(left)
-    right_list.append(right)
+    while True:
+        line = input()
+        if not line:
+            break
 
+        left, right = map(int, line.split())
+        left_list.append(left)
+        right_list.append(right)
 
-left_list.sort()
-right_list.sort()
+    similarity_score = 0
 
-sum_difference = 0
-similarity_score = 0
+    for i in range(len(set(left_list))):
+        similarity_score += left_list[i] * right_list.count(left_list[i])
 
-for i in range(len(left_list)):
-    sum_difference += abs(left_list[i] - right_list[i])
-
-for i in range(len(set(left_list))):
-    similarity_score += left_list[i] * right_list.count(left_list[i])
+    return similarity_score
 
 
-print(sum_difference)
-print(similarity_score)
+print(part1())
+print(part2())
