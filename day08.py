fc2587e7b26cabb3ca04e4576e8a0f6bc7254652
XDTerminated <gupta.sayam2006@gmail.com>
2026-05-23 23:09:36 -0500

    clean up day 8

--- day08.py
+++ day08.py
@@ -1,87 +1,54 @@
-data = []
-antinodes = set()
+from collections import defaultdict
+from itertools import combinations
 
-with open("input.txt") as f:
-    for line in f:
-        data.append(line.strip())
 
-d = {}
+def take_input():
+    grid = []
+    while line := input():
+        grid.append(line)
+    return grid
 
-for i in range(len(data)):
-    for j in range(len(data[i])):
-        if data[i][j] == ".":
-            continue
-        elif data[i][j] not in d.keys():
-            d[data[i][j]] = [(i, j)]
-        else:
-            d[data[i][j]].append((i, j))
 
-for key in d.keys():
-    for i in range(len(d[key])):
-        for j in range(len(d[key])):
-            if i == j:
-                continue
-            dist_x = d[key][i][0] - d[key][j][0]
-            dist_y = d[key][i][1] - d[key][j][1]
+def antennas_by_freq(grid):
+    by_freq = defaultdict(list)
+    for i, row in enumerate(grid):
+        for j, c in enumerate(row):
+            if c != ".":
+                by_freq[c].append((i, j))
+    return by_freq
 
-            ant = (dist_x + d[key][i][0], dist_y + d[key][i][1])
-            ant2 = (d[key][j][0] - dist_x, d[key][j][1] - dist_y)
 
-            if not (
-                ant[0] < 0
-                or ant[0] >= len(data)
-                or ant[1] < 0
-                or ant[1] >= len(data[0])
-            ):
-                antinodes.add(ant)
-            if not (
-                ant2[0] < 0
-                or ant2[0] >= len(data)
-                or ant2[1] < 0
-                or ant2[1] >= len(data[0])
-            ):
-                antinodes.add(ant2)
+def in_bounds(grid, pos):
+    return 0 <= pos[0] < len(grid) and 0 <= pos[1] < len(grid[0])
 
-print(len(antinodes))
 
-for key in d.keys():
-    for i in range(len(d[key])):
-        for j in range(len(d[key])):
-            if i == j:
-                continue
-            dist_x = d[key][i][0] - d[key][j][0]
-            dist_y = d[key][i][1] - d[key][j][1]
+def part1(grid):
+    antinodes = set()
+    for positions in antennas_by_freq(grid).values():
+        for a, b in combinations(positions, 2):
+            di, dj = a[0] - b[0], a[1] - b[1]
+            for p in [(a[0] + di, a[1] + dj), (b[0] - di, b[1] - dj)]:
+                if in_bounds(grid, p):
+                    antinodes.add(p)
+    return len(antinodes)
 
-            count = 0
 
-            while True:
-                ant = ((count * dist_x) + d[key][i][0], (count * dist_y) + d[key][i][1])
-                if not (
-                    ant[0] < 0
-                    or ant[0] >= len(data)
-                    or ant[1] < 0
-                    or ant[1] >= len(data[0])
-                ):
-                    antinodes.add(ant)
-                    count += 1
-                else:
-                    break
+def part2(grid):
+    antinodes = set()
+    for positions in antennas_by_freq(grid).values():
+        for a, b in combinations(positions, 2):
+            di, dj = a[0] - b[0], a[1] - b[1]
+            i, j = a
+            while in_bounds(grid, (i, j)):
+                antinodes.add((i, j))
+                i, j = i + di, j + dj
+            i, j = b
+            while in_bounds(grid, (i, j)):
+                antinodes.add((i, j))
+                i, j = i - di, j - dj
+    return len(antinodes)
 
-            count = 0
-            while True:
-                ant2 = (
-                    d[key][j][0] - (count * dist_x),
-                    d[key][j][1] - (count * dist_y),
-                )
-                if not (
-                    ant2[0] < 0
-                    or ant2[0] >= len(data)
-                    or ant2[1] < 0
-                    or ant2[1] >= len(data[0])
-                ):
-                    antinodes.add(ant2)
-                    count += 1
-                else:
-                    break
 
-print(len(antinodes))
+grid = take_input()
+print(part1(grid))
+print(part2(grid))
